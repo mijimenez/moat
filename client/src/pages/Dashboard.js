03e244decc92fd7c7e6b3df060871077ae92c11f
pb-totoro <jonesec2@gmail.com>
2020-04-24 15:02:51 -0400

    reviewing with master

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -16,9 +16,9 @@ function Dashboard() {
     const [isSticky, setSticky] = useState(false);
     const ref = useRef(null);
     const handleScroll = () => {
-      if (ref.current) {
-        setSticky(ref.current.getBoundingClientRect().top <= 0);
-      }
+        if (ref.current) {
+            setSticky(ref.current.getBoundingClientRect().top <= 0);
+        }
 
     };
 
@@ -30,17 +30,17 @@ function Dashboard() {
         window.addEventListener('scroll', handleScroll);
 
         return () => {
-          window.removeEventListener('scroll', () => handleScroll);
+            window.removeEventListener('scroll', () => handleScroll);
         };
     }, [])
 
     function getTrending() {
         API.getTrending()
-        .then(res => {
-            // console.log(res.data);
-            setTrendingPosts(res.data)
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                // console.log(res.data);
+                setTrendingPosts(res.data)
+            })
+            .catch(err => console.log(err));
     }
     // console.log(trendingPosts);
     let usernameStored
@@ -49,66 +49,62 @@ function Dashboard() {
         console.log("usernameStored: " + usernameStored)
         console.log(usernameStored)
         API.getUserCategories(usernameStored)
-           .then(res => {
-              console.log(res.data)
-              setCategories(res.data)
-              console.log(userCategories)
-           })
-           .catch(err => console.log(err));
-     }
-     console.log(userCategories)
+            .then(res => {
+                console.log(res.data)
+                setCategories(res.data)
+                console.log(userCategories)
+            })
+            .catch(err => console.log(err));
+    }
+    console.log(userCategories)
 
 
     function handleCategorySelect(categoryPicked) {
         console.log(categoryPicked);
         API.getPostByCategories(categoryPicked)
-        .then(res => {
-            console.log(res.data);
-            setTrendingPosts(res.data)
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                console.log(res.data);
+                setTrendingPosts(res.data)
+            })
+            .catch(err => console.log(err));
     };
 
     return (
         <div className="container" id="dashboardPage" style={{ marginTop: "30px", marginBottom: "100px", minHeight: "100vh" }}>
             <div className="hero row p-5 mb-3">
-                {/* <div className="col-md-6">
-                    <Tagline lineNum={[{ 1: "Welcome to" }, 2]} />
-                </div>
-                <div className="col-md-6">
-                    <Tagline lineNum={[3]} />
-                </div> */}
                 <div className="col-12">
                     <Tagline />
                 </div>
             </div>
             <div className="row">
                 <div className={`sticky-wrapper${isSticky ? ' sticky' : ''}`} ref={ref}>
-                    {userCategories.length >0? (
-                    <List>
-                        <li className="list-group-item font-weight-bold">Get Trending</li>
-                        <br></br>
-                        <li className="list-group-item font-weight-bold">By Category</li>
-                        {userCategories.map(category => (
-                            <ListItem
-                                key={category.id}
-                                item={category}
-                                handleCategorySelect={handleCategorySelect}
-                                categoryPicked={category}
-                            />
-                        ))}
-                    </List>
-                    ) : (
-                        <p className="display-message text-center mt-5">No categories added. Add from categories tab to save search preferences.</p>
-                    )}
+                    <ul className="list-group">
+                        {userCategories.length > 0 ? (
+                            <List>
+                                <li className="list-group-item font-weight-bold">Get Trending</li>
+                                <br></br>
+                                <li className="list-group-item font-weight-bold">By Category</li>
+                                {userCategories.map(category => (
+                                    <ListItem
+                                        // key={category.id}
+                                        item={category}
+                                        handleCategorySelect={handleCategorySelect}
+                                        categoryPicked={category}
+                                    />
+                                ))}
+                            </List>
+                        ) : (
+                                <p className="display-message text-center mt-5">No categories added. Add from categories tab to save search preferences.<button> Here! </button></p>
+                            )}
+                    </ul>
                 </div>
                 <div className="trending">
                     <p className="mb-3 text-center font-weight-bold">Trending</p>
                     {
                         trendingPosts.length > 0 ? trendingPosts.map(post =>
-                        (< UserPost post={post} getTrending={getTrending} key={post._id} />)) : (
-                            <p className="display-message text-center mt-5">No one has created any posts yet!</p>
-                        )}   
+                            (< UserPost post={post} getTrending={getTrending} key={post._id} />)) : (
+                                <p className="display-message text-center mt-5">No one has created any posts yet!</p>
+                            )}
                 </div>
             </div>
         </div>
