7367aa4b96504ca4db06bf17822d14beac7c6ee2
pb-totoro <jonesec2@gmail.com>
2020-04-24 10:27:00 -0400

    updated depreciated urlparser, added userNewUrlParser

--- models/mongoose.js
+++ models/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 //your local database url
 //27017 is the default mongoDB port
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/moatDB' 
-mongoose.connect(uri).then(
+mongoose.connect(uri, { useNewUrlParser: true }).then(
     () => { 
         /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
         console.log('Connected to Mongo');
