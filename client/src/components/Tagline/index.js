44ab631c52e5e609a104a922a051cccf0cae6903
pb-totoro <jonesec2@gmail.com>
2020-04-25 14:03:58 -0400

    pushing to heroku

--- client/src/components/Tagline/index.js
+++ client/src/components/Tagline/index.js
@@ -2,23 +2,16 @@ import React from "react";
 // import "../sass/style.scss";
 
 function Tagline({ lineNum }) {
-    const tagArray = ["MOAT", "MASTER OF ALL TRADES", "A place for hobbyists and professionals to exchange advice."];
+    const tagArray = ["MOAT", "MASTER OF ALL TRADES", "A place for hobbyists and professionals to talk shop."];
+    console.log(tagArray);
     return (
-        // lineNum.map(num => {
-        //     return (
-        //         <div>
-        //             {
-        //                 Object.values(num).length > 0 ? Object.values(num).concat(` ${tagArray[Object.keys(num) - 1]}`) : tagArray[num - 1]
-        //             }
-        //         </div>
-        //     )
-        // })
+
         <div className="wrapper">
             <div className="welcome">
                 <h1 className="mb-2">Welcome to MOAT.</h1>
                 <h4 className="tagline">Master of All Trades</h4>
             </div>
-            <p>A place for hobbyists and professionals to exchange advice.</p>
+            <p>A place for hobbyists and professionals to talk shop.</p>
         </div>
     );
 }
