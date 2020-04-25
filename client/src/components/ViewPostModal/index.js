51ac9b933b2cf948ecd3bd1eaa858629936eb6ee
Madeline Jimenez <madelineijimenez@gmail.com>
2020-04-24 23:37:23 -0400

    Fix styling on comments

--- client/src/components/ViewPostModal/index.js
+++ client/src/components/ViewPostModal/index.js
@@ -85,7 +85,7 @@ function ViewPostModal({ post, modalId, commentsArray, getUser, getTrending }) {
 
     function renderComments(comments) {
         return (
-            <div className="modal-dialog" role="document">
+            <div className="modal-dialog mb-3" style={{ width: "90%", margin: "auto" }} role="document">
                 {comments.map(comment => (
                     <div className="modal-content" key={comment._id}>
 
