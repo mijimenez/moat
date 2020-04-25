6e3788203d2d126577381725be106773c795e8a8
pb-totoro <jonesec2@gmail.com>
2020-04-25 14:26:10 -0400

    adding console.log

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -112,6 +112,7 @@ function Account() {
 
         API.uploadPhoto(formdata)
             .then(res => {
+                console.log( `--------------- \n  here is res  \n ------------ ${JSON.stringify(res.data, null,4)} \n --------------` )
                 console.log("Successfully uploaded profile pic!");
                 console.log("picture: " + JSON.stringify(res.data.profilePicture));
                 localStorage.setItem("profilePicMOAT", res.data.profilePicture.replace(/\\/gi, "/"));
