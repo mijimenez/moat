a539d56789d589cc5f6375a63ce326ca37aaec5a
Madeline Jimenez <madelineijimenez@gmail.com>
2020-04-24 23:25:25 -0400

    Fix styling for nothing added messages

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -105,7 +105,8 @@ function Dashboard() {
                                 <ListItem
                                 item={
                                     <Link to="/categories">
-                                        <span>No categories added. Click here to add categories.</span>
+                                        <span>No categories added.</span><br></br>
+                                        <span>Click here to add categories.</span>
                                     </Link>
                                 }
                                 handleCategorySelect={filler}
